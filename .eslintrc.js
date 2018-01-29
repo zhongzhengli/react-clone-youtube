230c6533450f3036c18ee49097711b9412a33fa3
Zhong Zheng Li <zhongzhengli.taiwan@gmail.com>
2018-01-29 21:52:34 +0800

    add video_list, fix crlf -> lf, fix eslint.js -> eslint.json

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "parser": "babel-eslint",
-    "extends": "google",
-    "rules": {
-        // windows linebreaks when not in production environment
-        "linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"],
-        "no-unused-vars": 1
-    }
-};
---
+++ src/components/video_list.js
@@ -0,0 +1,8 @@
+const VideoList = () => {
+  return (
+    <ul className="col-md-4 list-group">
+    </ul>
+  );
+};
+
+export default VideoList;
